80b3888fca44acc340794c2988ace7d625e6adc4
bertalan83 <e.bertalan96@gmail.com>
2022-03-16 19:08:06 +0100

    Sajat fajl keszitese

--- src/main/Bertalan.java
+++ src/main/Bertalan.java
@@ -100,19 +100,94 @@ public class Bertalan {
                 Ha 3-mal és 5-tel is osztható, akkor pedig azt, hogy "fizzbuzz"
          */
 
-       /* int from = 10;
+      /* int from = 10;
         int to = 33;
 
         while (from <= to) {
-            from ++;
             if (from%3==0){
                 System.out.println("fizz");
             }else if (from%5==0){
                 System.out.println("buzz");
-            }else if (from%15==0){
+            }else if (from%3==0 && from%5==0){
                 System.out.println("fizzbuzz");
+            }from ++;
+        }*/
+
+        // 7. feladat ---------------------------------------------------------------------------------
+        /* TODO
+            Generálj random számot 0 és 10 között (mindkettő beleértendő)!
+         */
+       /* double random1 = Math.random();         // random1 >= 0.0 && random1 < 1.0
+        int randomInt = (int) (Math.random() * 10 + 0);     // randomInt lehet: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        int randomInt2 = (int) (Math.random() * 11);     // randomInt2 lehet: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+        long randomLong = (long) (Math.random() * Long.MAX_VALUE);*/
+
+        // 8. feladat ---------------------------------------------------------------------------------
+        /* TODO
+            Generálj random számot 5 és 10 között!
+         */
+
+        /*int min = 5;
+        int max = 11;
+
+        for (int i = 0; i < 1000; i++) {
+            int random2 = (int) (Math.random() * (max - min) + min);    // érték tartományt adsz meg
+
+            if (random2 < 5 || random2 > 10) {
+                System.out.println("rossz szám! " + random2);
             }
         }
-*/
+
+        System.out.println("ok");*/
+
+        // 9. feladat ---------------------------------------------------------------------------------
+        /* TODO
+            Generálj random számot 1 és 6 között - beleértendő a két szélső értéket is.
+            (Tehát a random szám értéke lehet: 1, 2, 3, 4, 5, 6)
+            Ellenőrizd, hogy a szám valóban 1 és 6 közötti-e!
+            (Például úgy, mint az előző feladatnál csináltuk.)
+            Egészítsd ki az ellenőrzést azzal, hogy megnézed:
+                - generált-e a program 1-et
+                - generált-e a program 6-ot
+         */
+
+      /* int min = 0;
+        int random = (int) (Math.random() * (7 - 1) + 1);
+        int max = 0;
+
+        for (int i = 0; i < 1000; i++){
+            if (random < 1 || random > 6){
+                System.out.println("Rossz szam! " + random);
+            }
+            if (random==1){
+                min++;
+            } else if ( random==6 ){
+                max++;
+            }
+        }if (min > 0){
+        System.out.println("1-et generalt");}
+        else if (max > 0){
+        System.out.println("6-t generalt");}
+        System.out.println("OK");*/
+
+        // 10. feladat ---------------------------------------------------------------------------------
+        /* TODO
+            Generálj 100-szor egy random számot 1 és 6 között - beleértendő a két szélső értéket is.
+            Számold meg, hogy melyik számot hányszor generálta a programod!
+            A végén írd ki az eredményt a képernyőre!
+         */
+
+      /*  int[] randomCounter = new int[6];
+
+        for (int i = 0; i < 100; i++) {
+            int random = (int) (Math.random() * (7 - 1) + 1);
+            randomCounter[random - 1] += 1;
+        }
+        for (int i = 0; i < randomCounter.length; i++) {
+            System.out.println((i + 1) + " generated " + randomCounter[i] + " times");
+        }*/
+
+
     }
 }

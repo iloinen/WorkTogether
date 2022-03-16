a4f17e0cdcb8132f3323e907b423900296f086d0
Moraq99 <kukucskamacska@gmail.com>
2022-03-16 19:28:35 +0100

    Feladatok 1

--- src/main/barni.java
+++ src/main/barni.java
@@ -7,6 +7,19 @@ public class barni {
         Scanner sc = new Scanner(System.in);
 
 
+        System.out.println("Kérel a színt!");
+        String light = sc.nextLine();
+
+        if (light.equalsIgnoreCase("zöld")) {
+            System.out.println("SZABAD!");
+        } else if (light.equalsIgnoreCase("sárga")) {
+            System.out.println("LASSÍTS!");
+        } else if (light.equalsIgnoreCase("piros")) {
+            System.out.println("MEGÁLLJ!");
+        } else {
+            System.out.println("NEM TUDOM!");
+        }
+
 
     }
 }
